c1c246bdcef5d1eb35427ef2522c906ecc889f97
Guillaume Baptist <guillaume.baptist@free.fr>
2018-12-24 13:27:08 +0100

    juste avant la modification du template register avec le quickform de boostrap

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 from config import Config
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -10,5 +11,7 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
+
+bootstrap = Bootstrap(app)
 
 from app import routes, models

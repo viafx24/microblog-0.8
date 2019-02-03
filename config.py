58ca025046ba6bc3de76a618e2047092bb6be47a
Guillaume Baptist <guillaume.baptist@free.fr>
2019-02-03 10:21:49 +0100

    test new secret key

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config(object):
-    SECRET_KEY = os.environ.get('SECRET_KEY') or '\xfd{H\xe9<\x95\xx9\xe3\x96.1\xd1\x0hO<!'
+    SECRET_KEY = os.environ.get('SECRET_KEY') or '#5fbd9d}QE|c*i[Ysh8kCsjgf(jtIe'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False

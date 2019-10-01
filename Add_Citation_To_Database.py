30c603c923d4dc0000bc24555248897baeb3a4ea
Guillaume Baptist <guillaume.baptist@free.fr>
2019-10-01 20:28:02 +0200

    add new batrch of citations to 38

--- Add_Citation_To_Database.py
+++ Add_Citation_To_Database.py
@@ -9,54 +9,75 @@ from app.models import User, Citation
 
 #print(Citation.query.get(300))
 
-
-
-
-
-
-
-
 # ajout d'un batch de 10 citations;
 
 u = User.query.get(1)
 
-citation = Citation(text='Dans le monde, il y a toujours des gens qui ne supportent pas la lumière. C’est parce qu’ils sont eux-mêmes remplis d’ombre.',SRR=1,TRT=1,author=u)
+citation = Citation(text='Bizarre qu’au sein de ce pitoyable spectacle, je n’ai éprouvé nulle haine, bien que les individus de cette espèce m’aient empoisonné la vie douze année durant.',SRR=1,TRT=1,author=u)
 db.session.add(citation)
 db.session.commit()
 
-citation = Citation(text='Jamais il ne fait au méchant l’honneur de croire que la raison ait inspiré un seul de ses actes.',SRR=1,TRT=1,author=u)
+citation = Citation(text='Pourquoi l’individu a-t-il les vices de la société ? La société les lui donne. On est entrainé dans le faux par des parents, par des voisins. On apprend le mal, ensuite on l’enseigne et on arrive à ce comble de dépravation qui concentre dans un seul cœur la science perverse de tous.',SRR=1,TRT=1,author=u)
 db.session.add(citation)
 db.session.commit()
 
-citation = Citation(text='Il n’est pas naturel que celui qui a la force désire beaucoup le pouvoir ; et l’on a souvent remarqué que les athlètes sont rarement méchants.',SRR=1,TRT=1,author=u)
+citation = Citation(text='Le pire qui puisse advenir est de se mettre dans son tort par rapport aux canailles ; on se fait alors chapitrer par eux, et il n’est juge plus impitoyable que celui qui, premièrement, est dans son droit et deuxièmement, est un coquin.',SRR=1,TRT=1,author=u)
 db.session.add(citation)
 db.session.commit()
 
-citation = Citation(text='Le guerrier est un métaphysicien. Le guerrier s’est dessiné un dieu, une justice, des maximes, un ordre humain qu’il croit surhumain. Par un retour sur lui-même que tout homme connaît, il honore en lui-même, plus que tout, ce pouvoir de trouver la loi et de la suivre. D’où la pire injure qu’on puisse lui faire c’est de penser autrement que lui, c’est de vivre d’après d’autres maximes que les siennes ; c’est de mépriser ce qu’il honore.',SRR=1,TRT=1,author=u)
+citation = Citation(text='Chacun sera sot autant qu’il est roi. Exactement autant qu’il fera faire, au lieu de faire.',SRR=1,TRT=1,author=u)
 db.session.add(citation)
 db.session.commit()
 
-citation = Citation(text='Le courage vraiment sûr est celui qui s’observe beaucoup et longtemps, qui se couvre d’abord et n’avance qu’à pas lents et calculés.',SRR=1,TRT=1,author=u)
+citation = Citation(text='Il faut une sagesse supérieure pour ne rien supposer jamais des intentions et des pensées d’un homme.',SRR=1,TRT=1,author=u)
 db.session.add(citation)
 db.session.commit()
 
-citation = Citation(text='L’effort excessif peut aussi résulter de l’impatience ou de l’exaltation, deux états qui ne mènent nulle part. De même qu’il faut de la patience pour faire pousser une récolte, il ne sert à rien de tirer sur les plans pour les faire sortir plus vite.',SRR=1,TRT=1,author=u)
+citation = Citation(text='Les êtres se comportent de façon nuisible parce qu’ils sont sous l’emprise de l’ignorance et des poisons mentaux que celle-ci engendre.',SRR=1,TRT=1,author=u)
 db.session.add(citation)
 db.session.commit()
 
-citation = Citation(text='Ce qui est venu par la fatigue s’en va par le repos.',SRR=1,TRT=1,author=u)
+citation = Citation(text='L’ignorance est la cause de tous les maux.',SRR=1,TRT=1,author=u)
 db.session.add(citation)
 db.session.commit()
 
-citation = Citation(text='C’est pourtant bien à notre esprit que nous avons affaire du matin au soir, et c’est lui qui en fin de compte, détermine la qualité de chaque instant de notre existence. Nos pensées détiennent ainsi l’immense pouvoir de conditionner notre manière d’être.',SRR=1,TRT=1,author=u)
+citation = Citation(text='L’homme réellement fort se sent également responsable de son ennemi.',SRR=1,TRT=1,author=u)
 db.session.add(citation)
 db.session.commit()
 
-citation = Citation(text='Ce sont les autres, tous les autres, qui fondent la trame de nos vies et forment la matière de nos existences.',SRR=1,TRT=1,author=u)
+citation = Citation(text='La tendance à nuire, à blâmer et à dénigrer est une forme de vanité.',SRR=1,TRT=1,author=u)
 db.session.add(citation)
 db.session.commit()
 
-citation = Citation(text='Heureux par-dessus tout celui qui sent la trace de son coup de marteau sur le loquet de sa porte.',SRR=1,TRT=1,author=u)
+citation = Citation(text='Il faut savoir étiqueter une pensée ou un état d’esprit en « lamentation ».',SRR=1,TRT=1,author=u)
+db.session.add(citation)
+db.session.commit()
+
+citation = Citation(text='Quand rien ne va plus dedans, rien ne va plus dehors. La paix de l’âme est si importante qu’il faut savoir renoncer à ce que l’on entreprend pour ne pas la perdre.',SRR=1,TRT=1,author=u)
+db.session.add(citation)
+db.session.commit()
+
+citation = Citation(text='Du seul fait de rester assis à observer à quelle vitesse et avec quel illogisme, mes pensées et mes émotions allaient et venaient, je commençais à voir qu’elles n’étaient pas aussi solides et réelles qu’elles en avaient l’air. Alors, je commençais à lâcher prise sur ma croyance à l’histoire qu’elles avaient l’air de me raconter.',SRR=1,TRT=1,author=u)
+db.session.add(citation)
+db.session.commit()
+
+citation = Citation(text='Le mal qui nous afflige puise généralement sa force dans le rétrécissement de notre univers mental. Les pensées ne cessent alors de rebondir contre les parois de cette prison intérieure. Elles s’accélèrent et s’amplifient, chaque rebond nous infligeant de nouvelles meurtrissures. Il faut donc élargir notre horizon intérieur, au point que l’émotion n’ai plus de murs où ricocher sans trêve. Alors les projectiles du malheur vont se perdre dans le vaste espace de la liberté intérieure.',SRR=1,TRT=1,author=u)
+db.session.add(citation)
+db.session.commit()
+
+citation = Citation(text='Si parfois je veux m’amuser d’un fou, je n’ai pas loin à chercher, c’est de moi que je rie.',SRR=1,TRT=1,author=u)
+db.session.add(citation)
+db.session.commit()
+
+citation = Citation(text='Pendant l’hiver, le gel fige les lacs et les rivières. Avec le printemps, la terre et les eaux se réchauffent : c’est le dégel. Que reste-il alors de la dureté de la glace ? L’eau est douce et fluide, la glace dure et coupante. On ne peut pas dire qu’elles soient identiques ni non plus qu’elles soient différentes, car la glace n’est que de l’eau solidifiée et l’eau de la glace fondue. Il en est de même de nos perceptions du monde extérieur.',SRR=1,TRT=1,author=u)
+db.session.add(citation)
+db.session.commit()
+
+citation = Citation(text='Je vous souhaite la bonne humeur. Voilà ce qu’il faudrait offrir et recevoir. Voilà la vraie politesse qui enrichit tout le monde et d’abord celui qui donne. Voilà le trésor qui se multiplie par l’échange. On peut la semer le long des rues, dans les tramways, dans les kiosques à journaux, il ne s’en perdra pas un atome. Elle poussera et fleurira partout où vous l’aurez jeté.',SRR=1,TRT=1,author=u)
+db.session.add(citation)
+db.session.commit()
+
+citation = Citation(text='Fais que je ne cherche pas tant à être consolé que de consoler, d’être compris que de comprendre, d’être aimé que d’aimer. Parce que c’est en donnant que l’on reçoit. C’est en s’oubliant soi-même qu’on se retrouve.',SRR=1,TRT=1,author=u)
 db.session.add(citation)
 db.session.commit()
 

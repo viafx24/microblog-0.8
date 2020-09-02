ff408d13a5ca33787bf933678b441e1bcc3b98cb
Guillaume Baptist <guillaume.baptist@free.fr>
2020-09-02 12:10:42 +0200

    add citation 362 to 370 and test github because i moved the main directory of microblog 0.8 in onedrive/visual studio folder

--- Add_Citation_To_Database.py
+++ Add_Citation_To_Database.py
@@ -13,33 +13,43 @@ from app.models import User, Citation
 
 u = User.query.get(1)
 
-citation = Citation(text='Hâtons-nous de suivre le conseil de Nietzsche pour qui le meilleur moyen de bien inaugurer la journée consiste à se demander, dès son réveil, si aujourd’hui l’on peut faire plaisir à « au moins un homme ». Tout commence par le prochain.',SRR=1,TRT=1,author=u)
+citation = Citation(text='Je tiens note de ma dépense. Je ne puis me flatter de ne rien perdre ; mais ce que je perds et le pourquoi et le comment, je puis le dire, je puis rendre compte de ma gêne.',SRR=1,TRT=1,author=u)
 db.session.add(citation)
 db.session.commit()
 
-citation = Citation(text='Quelle que soit la personne que tu regardes, sache qu’elle a déjà plusieurs fois traversé l’enfer.',SRR=1,TRT=1,author=u)
+citation = Citation(text='Il ne faut pas de leçons pour se résoudre à coucher au besoin sur des roses. Il en faut pour s’endurcir aux tortures et n’y point subordonner sa foi.',SRR=1,TRT=1,author=u)
 db.session.add(citation)
 db.session.commit()
 
-citation = Citation(text='Les mots de Spinoza disent l’essentiel et me servent de programme : « bien faire et se tenir en joie ».',SRR=1,TRT=1,author=u)
+citation = Citation(text='Quand on ne va pas bien, on fait de l’archéologie et on finit par déterrer les fragments du passé. Fragments désignés d’emblée comme l’origine du chaos actuel.',SRR=1,TRT=1,author=u)
 db.session.add(citation)
 db.session.commit()
 
-citation = Citation(text='On progresse beaucoup plus en écoutant qu’en parlant. Le proverbe dit : « tu as 2 oreilles et une bouche, ce qui veut dire que tu dois écouter 2 fois plus que tu ne dois parler ».',SRR=1,TRT=1,author=u)
+citation = Citation(text='La colère avec sa racine empoisonnée et sa pointe de miel.',SRR=1,TRT=1,author=u)
 db.session.add(citation)
 db.session.commit()
 
-citation = Citation(text='Il faut que l’ego s’use comme une vieille chaussure, voyageant de la souffrance à la libération.',SRR=1,TRT=1,author=u)
+citation = Citation(text='Le grand obstacle c’est toujours la représentation et non la réalité.',SRR=1,TRT=1,author=u)
 db.session.add(citation)
 db.session.commit()
 
-citation = Citation(text='On ne peut être vraiment paisible si l’on ne porte en soi la qualité invincible de la paix.',SRR=1,TRT=1,author=u)
+citation = Citation(text='En nous résignant à être en permanence la victime de nos pensées, nous ressemblons au chien qui court après chaque pierre qu’on lui lance. Pourtant si nous examinons la situation avec un peu de recul, nous lui trouverons très souvent un aspect comique : en proie aux tourments de l’ego, nous sommes pareils à un gamin qui trépigne de rage parce qu’on a contrarié ses caprices. Alors, nous ne sommes plus le chien courant après chaque pierre, mais le Lion à qui on ne peut en lancer qu’une, car au lieu de la poursuivre, il se retourne vers le lanceur.',SRR=1,TRT=1,author=u)
 db.session.add(citation)
 db.session.commit()
 
-citation = Citation(text='Vivre c’est être utile à plusieurs.',SRR=1,TRT=1,author=u)
+citation = Citation(text='Epictète recommandait à ses disciples de s’exercer d’abord dans les petites choses, afin de créer une habitude. Les bonheurs répétés sont souvent le fruit d’une ascèse qui, au sens étymologique, signifie « exercice » en grec.',SRR=1,TRT=1,author=u)
 db.session.add(citation)
 db.session.commit()
+
+citation = Citation(text='Il est facile d’être patient quand tout va bien. Ce sont justement les situations difficiles qui nous mettent au défi de renforcer cette vertu cardinale qu’est la patience. Se souvenir d’en faire une pratique régulière.',SRR=1,TRT=1,author=u)
+db.session.add(citation)
+db.session.commit()
+
+citation = Citation(text='Le rire soulève une des plus légères affections de l’âme ; il ne voit rien de grand, de sévère ni même de sérieux dans tout notre vain appareil.',SRR=1,TRT=1,author=u)
+db.session.add(citation)
+db.session.commit()
+
+
 
 citations=Citation.query.order_by(Citation.number).filter(Citation.number>=299).all()
 print(citations)
